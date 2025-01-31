aaf71b9d10aa9ca50d02b4287ad40eef09fdb92a
Giuseppe <napoligiuseppe@web.de>
2025-01-31 12:30:44 +0100

    ExplosiveBarrel, Interface

--- Source/HordeMultiplayer/HMPProjectile.cpp
+++ Source/HordeMultiplayer/HMPProjectile.cpp
@@ -17,13 +17,14 @@ AHMPProjectile::AHMPProjectile()
 	PrimaryActorTick.bCanEverTick = true;
 
 	SphereComp = CreateDefaultSubobject<USphereComponent>("SphereComp");
+	SphereComp->SetCollisionProfileName("Projectile");
 	RootComponent = SphereComp;
 
 	EffectComp = CreateDefaultSubobject<UNiagaraComponent>("EffectComp");
 	EffectComp->SetupAttachment(SphereComp);
 
 	MovementComp = CreateDefaultSubobject<UProjectileMovementComponent>("MovementComp");
-	MovementComp->InitialSpeed = 100.0f;
+	MovementComp->InitialSpeed = 1000.0f;
 	MovementComp->bRotationFollowsVelocity = true;
 	MovementComp->bInitialVelocityInLocalSpace = true;
 
---
+++ Source/HordeMultiplayer/Private/HMP_Gameplay_Interface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HMP_Gameplay_Interface.h"
+
+// Add default functionality here for any IHMP_Gameplay_Interface functions that are not pure virtual.
